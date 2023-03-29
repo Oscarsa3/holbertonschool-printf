5af7a046e93534933dd788fc872fc985b9c68b40
Oscarsa3 <oscarorlandosalinassilvestre>
2023-03-29 14:40:17 -0700

    Adding obfunctions for printf clone

---
+++ ob_functions.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * ob_funtions - obtenemos la funcion deseada
+  * @s: caracter de la funcion
+  * Return: funcion
+  */
+int (*ob_funtions(const char *s))(va_list)
+{
+	int i = 0;
+	verifica a[] = {
+		{"c", fun_c},
+		{"s", fun_s},
+		{NULL, NULL}
+	};
+	while (a[i].letra)
+	{
+		if (*s == *(a[i].letra))
+		{
+			return (a[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+
+}
