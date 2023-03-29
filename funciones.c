9d6fea994c56c0075e9f8926ddeae350ccdf211a
Nilesoj20 <jvrn2014@gmail.com>
2023-03-29 14:34:36 -0700

    feat: adding file funciones.c

---
+++ funciones.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* fun_c - funcion carcter
+* @args: lista
+* Return: 1
+*/
+int fun_c(va_list args)
+{
+	char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
+/**
+ * fun_s - funcion string
+ * @args: lista
+ * Return: size
+ */
+int fun_s(va_list args)
+{
+	int i = 0;
+	char *s;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+	return (0);
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
