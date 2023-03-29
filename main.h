b1109da162de98cd3b1e54428c420d5cf0b1b31c
Nilesoj20 <jvrn2014@gmail.com>
2023-03-29 14:58:35 -0700

    updating file main.h

--- main.h
+++ main.h
@@ -1,5 +1,21 @@
 #ifndef HEADER
 #define HEADER
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+int (*ob_funtions(const char *s))(va_list);
+int fun_c(va_list);
+int fun_s(va_list);
+/**
+* struct verificar - para verificar el especificador
+* @letra: verificamos el caracter para ver si llamamos su funcion
+* @f: funtion pointer
+*/
+typedef struct verificar
+{
+	char *letra;
+	int (*f)(va_list);
+} verifica;
 #endif
